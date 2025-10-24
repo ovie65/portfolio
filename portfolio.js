7e251ccb560225f52534ebaadae907f702043c59
ovie <divineovie3@mail.com>
2025-10-24 06:38:09 -0700

    first commit

---
+++ portfolio.js
@@ -0,0 +1,14 @@
+ VANTA.NET({
+      el: "#vanta-bg",
+      mouseControls: true,
+      touchControls: true,
+      minHeight: 200.00,
+      minWidth: 200.00,
+      scale: 1.00,
+      scaleMobile: 1.00,
+      color: 0x00ff00,
+      backgroundColor: 0x1e1f25,
+      points: 12.00,
+      maxDistance: 25.00,
+      spacing: 18.00
+    })
