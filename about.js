02175553960f0fc52389a5cb1fce974e8376e39d
ovie <divineovie3@mail.com>
2026-02-18 03:38:47 -0800

    first commit

--- about.js
+++ about.js
@@ -1,1 +1,62 @@
+document.addEventListener("DOMContentLoaded", function () {
+
+  gsap.registerPlugin(TextPlugin);
+
+  const tl = gsap.timeline();
+
+  tl.to(".ki", {
+    duration: 2,
+    text: "ABOUT ME",
+    ease: "none"
+  })
+
+  .to(".ki", {
+    duration: 1.5,
+    text: "",
+    ease: "none"
+  })
+
+  .to(".ki", {
+    duration: 2.5,
+    text: "Welcome ",
+    ease: "none"
+  })
+
+  .to(".ki", {
+    duration: 1.5,
+    text: "",
+    ease: "none"
+  })
+
+  .to(".ki", {
+    duration: 2,
+    text: "ABOUT ME",
+    ease: "none"
+  });
+
+});
+
+
+
+
+
+document.addEventListener("DOMContentLoaded", () => {
+
+  gsap.registerPlugin(ScrollTrigger);
+
+  gsap.from(".form-container", {
+    x: -120,
+    opacity: 0,
+    duration: 1.2,
+    ease: "power3.out",
+    scrollTrigger: {
+      trigger: ".form-container",
+      start: "top 80%",
+      toggleActions: "play reverse play reverse",
+    }
+  });
+
+});
+
+
 
